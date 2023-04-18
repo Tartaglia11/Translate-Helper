a71110ca21d611f0860f6bc979a5961007fd9dde
Tartaglia11 <wilax175@gmail.com>
2023-04-18 17:30:16 +0600

    put 'dictionary.txt' content in java array

--- src/com/translate/Main.java
+++ src/com/translate/Main.java
@@ -1,7 +1,24 @@
 package com.translate;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        File file = new File("E:\\TranslateHelper\\dictionary.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        ArrayList<String> words = new ArrayList<>();
+
+        while ((st = br.readLine()) != null) {
+
+            words.add(st);
+        }
+
+        System.out.println(Arrays.toString(words.toArray()));
     }
 }
