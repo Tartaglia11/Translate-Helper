734bef9bb1754632cb12700329d6a9f8cc20f5fa
Tartaglia11 <wilax175@gmail.com>
2023-04-26 21:20:16 +0600

    now you can write a translation without taking into account the capitalization of letters

--- src/com/translate/Main.java
+++ src/com/translate/Main.java
@@ -25,11 +25,14 @@ public class Main {
         StringBuilder ruWord = new StringBuilder(result.reverse().substring(0, result.indexOf("-")));
         ruWord = new StringBuilder(ruWord).reverse();
 
+        ruWord = new StringBuilder(firstUpperCase(String.valueOf(ruWord)));
+
         System.out.println("Word: " + ruWord.toString().trim());
         System.out.print("Enter the translate: ");
         Scanner sc = new Scanner(System.in);
 
         String translate = sc.nextLine().trim();
+        translate = firstUpperCase(translate);
 
         if (enWord.equals(translate)) {
 
@@ -38,5 +41,11 @@ public class Main {
 
             System.out.println("It's wrong!");
         }
+    }
+
+    public static String firstUpperCase(String word){
+
+        if(word == null || word.isEmpty()) return "";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 }
