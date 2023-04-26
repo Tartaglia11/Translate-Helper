2708007f47f01bdb8fc1451b81cb7fee2bc694b0
Tartaglia11 <wilax175@gmail.com>
2023-04-26 21:13:56 +0600

    created the first version

--- src/com/translate/Main.java
+++ src/com/translate/Main.java
@@ -1,24 +1,42 @@
 package com.translate;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         File file = new File("E:\\TranslateHelper\\dictionary.txt");
+        ArrayList<String> words = new ArrayList<>();
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         String st;
 
-        ArrayList<String> words = new ArrayList<>();
 
         while ((st = br.readLine()) != null) {
 
             words.add(st);
         }
 
-        System.out.println(Arrays.toString(words.toArray()));
+        int randomWord = new Random().nextInt(words.size());
+
+        StringBuilder result = new StringBuilder(words.get(randomWord));
+        String enWord = result.substring(0, result.indexOf("-")).trim();
+        StringBuilder ruWord = new StringBuilder(result.reverse().substring(0, result.indexOf("-")));
+        ruWord = new StringBuilder(ruWord).reverse();
+
+        System.out.println("Word: " + ruWord.toString().trim());
+        System.out.print("Enter the translate: ");
+        Scanner sc = new Scanner(System.in);
+
+        String translate = sc.nextLine().trim();
+
+        if (enWord.equals(translate)) {
+
+            System.out.println("Right!");
+        } else {
+
+            System.out.println("It's wrong!");
+        }
     }
 }
